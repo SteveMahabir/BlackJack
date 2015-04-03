ecc1701f3c38b70ab5f1005ef6bba9e4e303f77d
Nickademus <adm.nickademus@gmail.com>
2015-04-03 13:14:22 -0400

    mainWindow

--- BlackJack/MainWindow.xaml.cs
+++ BlackJack/MainWindow.xaml.cs
@@ -26,7 +26,47 @@ namespace BlackJack
         }
 
 
-        
+
+        public enum round { bet = 0, deal, play, dealer, win };
+        public void disableUIforRound( round r ) {
+            switch (r)
+            {
+                case round.bet:
+                    btn_Bet.IsEnabled = true;
+                    sldr_BetAmount.IsEnabled = true;
+                    btn_Hit.IsEnabled = false;
+                    btn_Stay.IsEnabled = false;
+                    break;
+                case round.deal:
+                    btn_Bet.IsEnabled = false;
+                    sldr_BetAmount.IsEnabled = false;
+                    break;
+                case round.play:
+                     btn_Hit.IsEnabled = true;
+                     btn_Stay.IsEnabled = true;
+                    break;
+                case round.dealer:
+                    btn_Hit.IsEnabled = false;
+                    btn_Stay.IsEnabled = false;
+                    btn_Bet.IsEnabled = false;
+                    sldr_BetAmount.IsEnabled = false;
+                    break;
+                case round.win:
+                    btn_Hit.IsEnabled = false;
+                    btn_Stay.IsEnabled = false;
+                    btn_Bet.IsEnabled = false;
+                    sldr_BetAmount.IsEnabled = false;
+                    break;
+                default:
+                    break;
+            }
+        }
 
     }
+
+
+
+
+   
+
 }
